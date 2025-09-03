770dd2afb8d1a9f2c2adf62fc24d5b7b8dd31949
Thibault Caron <thibault.caron@laplateforme.io>
2025-09-03 08:58:28 +0200

    feat: jour01 job06

--- jour01/demo/src/main/java/com/example/demo/controller/HelloController.java
+++ jour01/demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,10 +1,9 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.beans.factory.annotation.Value;
-
 
 @Controller
 public class HelloController {
@@ -12,9 +11,12 @@ public class HelloController {
     @Value("${greeting.message}")
     private String greetingMessage;
 
+    @Value("${greeting.tools}")
+    private String toolsMessage;
+
     @GetMapping("/")
     @ResponseBody
     public String hello() {
-        return greetingMessage;
+        return greetingMessage + " | " + toolsMessage;
     }
 }
